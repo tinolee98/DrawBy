cf6288549fd789d1b952e80a888b76c583550053
kunholee_98 <dlrjsgh750@naver.com>
2021-09-17 16:20:56 +0900

    login

---
+++ users/login/login.resolvers.js
@@ -0,0 +1,40 @@
+import * as bcrypt from "bcrypt";
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+import jwt from "jsonwebtoken";
+
+export default {
+  Mutation: {
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({
+        where: {
+          username,
+        },
+        select: {
+          id: true,
+          username: true,
+          password: true,
+        },
+      });
+      if (!user) {
+        return {
+          ok: false,
+          error: "This username is not existed.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Password is incorrect.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        error: "test",
+        token,
+      };
+    },
+  },
+};
--- users/login/login.typeDefs.js
+++ users/login/login.typeDefs.js
@@ -1,7 +1,12 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type Query {
-    dummy: String
+  type LoginResult {
+    ok: Boolean!
+    error: String
+    token: String
+  }
+  type Mutation {
+    login(username: String!, password: String!): LoginResult!
   }
 `;
