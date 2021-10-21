cf812456e3035bba0ad4bd13ab02b645c0c59504
kunholee_98 <dlrjsgh750@naver.com>
2021-10-21 16:46:34 +0900

    me

---
+++ users/me/me.resolvers.js
@@ -0,0 +1,11 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    me: protectedResolver(
+      async (_, __, { loggedInUser }) =>
+        await client.user.findUnique({ where: { id: loggedInUser.id } })
+    ),
+  },
+};
