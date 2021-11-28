4b7b303688212a6b369d45d917d44a4963ea6d37
kunholee_98 <dlrjsgh750@naver.com>
2021-11-28 15:24:43 +0900

    댓글과 대댓글 타입 분리

--- comments/comments.resolvers.js
+++ comments/comments.resolvers.js
@@ -2,7 +2,16 @@ import client from "../client";
 
 export default {
   Comment: {
-    author: async ({ id }) => {
+    author: async ({ id, commentId }) => {
+      if (commentId) {
+        return await client.nestedComment
+          .findUnique({
+            where: {
+              id,
+            },
+          })
+          .user();
+      }
       return await client.comment
         .findUnique({
           where: {
@@ -11,7 +20,13 @@ export default {
         })
         .user();
     },
-    isMine: async ({ id }, _, { loggedInUser }) => {
+    isMine: async ({ id, commentId }, _, { loggedInUser }) => {
+      if (commentId) {
+        const nestedComment = await client.nestedComment.findUnique({
+          where: { id },
+        });
+        return nestedComment.userId === loggedInUser.id;
+      }
       const comment = await client.comment.findUnique({
         where: {
           id,
@@ -38,6 +53,42 @@ export default {
       const users = await client.likeCom.findMany({
         where: {
           commentId: id,
+        },
+        select: {
+          user: true,
+        },
+      });
+      const result = users.map((user) => user.user);
+      return result;
+    },
+  },
+  NestedComment: {
+    author: async ({ id }) => {
+      return await client.nestedComment
+        .findUnique({
+          where: {
+            id,
+          },
+        })
+        .user();
+    },
+    isMine: async ({ id }, _, { loggedInUser }) => {
+      const nestedComment = await client.nestedComment.findUnique({
+        where: { id },
+      });
+      return nestedComment.userId === loggedInUser.id;
+    },
+    totalLike: async ({ id }) => {
+      return client.likeNestCom.count({
+        where: {
+          nestedCommentId: id,
+        },
+      });
+    },
+    whoLikes: async ({ id }) => {
+      const users = await client.likeNestCom.findMany({
+        where: {
+          nestedCommentId: id,
         },
         select: {
           user: true,
